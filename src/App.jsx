a8abb3670529866e030a783d5a536db113bbcf53
Dellsony Dissanayaka <eng.dellsony@gmail.com>
2023-06-26 01:20:07 +0530

    relove the routins

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,32 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Box } from "@mui/material";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { Box } from "@mui/material";
 
+// import {
+//   ChannelDetail,
+//   VideoDetail,
+//   SearchFeed,
+//   Navbar,
+//   Feed,
+// } from "./components";
+
+// const App = () => (
+//   <BrowserRouter>
+//     <Box sx={{ backgroundColor: "#000" }}>
+//       <Navbar />
+//       <Routes>
+//         <Route exact path="/" element={<Feed />} />
+//         <Route path="/video/:id" element={<VideoDetail />} />
+//         <Route path="/channel/:id" element={<ChannelDetail />} />
+//         <Route path="/search/:searchTerm" element={<SearchFeed />} />
+//       </Routes>
+//     </Box>
+//   </BrowserRouter>
+// );
+
+// export default App;
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
 import {
   ChannelDetail,
   VideoDetail,
@@ -10,17 +36,17 @@ import {
 } from "./components";
 
 const App = () => (
-  <BrowserRouter>
+  <Router basename="/yt_clone_for_mern_developer">
     <Box sx={{ backgroundColor: "#000" }}>
       <Navbar />
       <Routes>
-        <Route exact path="/" element={<Feed />} />
+        <Route path="/" element={<Feed />} />
         <Route path="/video/:id" element={<VideoDetail />} />
         <Route path="/channel/:id" element={<ChannelDetail />} />
         <Route path="/search/:searchTerm" element={<SearchFeed />} />
       </Routes>
     </Box>
-  </BrowserRouter>
+  </Router>
 );
 
 export default App;
